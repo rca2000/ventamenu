b27539c8e41d4a7be228166259f4ba8096d95e92
rca2000 <rcornejo1972@gmail.com>
2015-05-18 02:21:06 +0000

    Actualizar GitHub

---
+++ js/fecha_actual.js
@@ -0,0 +1,21 @@
+//this function will find today's date
+function calendar(){
+    var day=['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sábado'];
+    var month=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Setiembre','Octubre','Noviembre','Diciembre'];
+    var d=new Date();
+    setText('calendar-day', day[d.getDay()]);
+    setText('calendar-date', d.getDate());
+    setText('calendar-month-year', month[d.getMonth()]+' '+(1900+d.getYear()));
+};
+
+//this function will set the text value of <p> tags
+function setText(id, val){
+    if(val < 10){
+        val = '0' + val;    //add leading 0 if val < 10
+    }
+    document.getElementById(id).innerHTML = val;
+};
+
+
+//call calendar() when page load
+window.onload = calendar;
